3820f9326fbf545822bf1f70ebd930a3456153a1
= <jlgurgelferandnes@gmail.com>
2021-12-08 10:27:33 -0300

    resposive correction Opacity Component

--- src/components/Find/Find.js
+++ src/components/Find/Find.js
@@ -146,7 +146,7 @@ const GridDiv = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
   gap: 30px;
-  margin: 100px;
+  margin: 50px;
 `;
 /*
 const style = {
